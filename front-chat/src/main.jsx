cfb33d2a83111eb2b87630761c3851504b55d254
MrCode <114862006+azhan0058@users.noreply.github.com>
2026-04-14 16:22:34 +0000

    h1

--- front-chat/src/main.jsx
+++ front-chat/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router";
+import {BrowserRouter, Route, Routes} from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-        <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/chat" element={<h1>This is chat app</h1>} />
+      </Routes> 
     </BrowserRouter>
   </StrictMode>,
 )
