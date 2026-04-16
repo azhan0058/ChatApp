4efcbace4742d65bccb45a67a39b3be7b6f9ecce
MrCode <114862006+azhan0058@users.noreply.github.com>
2026-04-16 15:30:36 +0000

    frontAlmostFinish

--- front-chat/src/components/ChatPage.jsx
+++ front-chat/src/components/ChatPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { MdAttachFile, MdSend } from "react-icons/md";
+
+
+const [messages, setMessages] = useState([]);
+const[input, setInput] = useState("");
+const inputRef = useRef(null);
+const chatBoxRef = useRef(null); 
+const[stompClient, setStompClient] = useState(null);
+const[roomId, setRoomId] = useState("");
 
 const ChatPage = () => {
   return (
-    <div>ChatPage made by me
+    <div>
         {/*this is header portion */}
         <header className='dark:border-gray-700 dark:bg-gray-800 shadow fixed w-full border rounded-lg flex px-2 py-4 gap-10 justify-around items-center '>
             {/*noom name div */}
@@ -22,7 +31,27 @@ const ChatPage = () => {
         </header>
 
         {/*this is chat portion*/}
-        <div>/</div>
+        <main className='py-20 w-2/3 dark:bg-slate-700 mx-auto  h-screen overflow-auto'> 
+            <div className='message-container'>
+                <h1>hi</h1>
+            </div>
+        </main>
+
+        {/*this is input message container portion */}
+        <div className="fixed bottom-5 w-full h-12">
+            <div className="h-full pr-8 gap-4 flex items-center justify-between w-1/2 mx-auto dark:bg-gray-900 shadow rounded ">
+              <input type="text" placeholder='type your text here' className='dark:border-gray-700 dark:bg-gray-800 px-5 py-2 rounded-full w-full  h-full focus:ring-0 focus:outline-none'/>
+              
+              <div className='flex gap-3'>
+                <button className='dark:bg-gray-700 px-3 py-2 border-0 rounded-full flex justify-center items-center'>
+                  <MdAttachFile size={20}/>
+                </button>
+                <button className='dark:bg-green-700 px-3 py-2 border-0 rounded-full flex justify-center items-center'>
+                  <MdSend size={20}/>
+                </button>
+              </div>
+            </div>
+        </div>
     </div>
   )
 }
